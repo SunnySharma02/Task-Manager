76640ce4a10338d6b299768f0ac2f00f7af50493
Sunny Sharma <sunnysharma@Sunnys-MacBook-Air.local>
2024-04-30 01:11:02 +0530

    tasks

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const taskAPI = require('./APIs/Task')
-const TaskModel = require('./models/TaskModel')
 const cors = require('cors')
 const path = require('path')
 
@@ -15,10 +14,6 @@ app.use(express.urlencoded())
 app.use(cors())
 app.use(express.static(path.resolve(process.env.PUBLIC_DIR)))
 
-// mongoose.connect(process.env.MONGO_URL)
-// .then(() => console.log("DB connected"))
-// .catch(err => console.log(err))
-
 async function main() {
     await mongoose.connect(process.env.MONGO_URL)
     console.log('Database Connected')
@@ -28,8 +23,6 @@ main().catch(err => console.log(err))
 
 
 app.use(taskAPI)
-
-// TaskModel.insertMany([{title: 'Gym', isComplete: true}, {title: 'Studying', isComplete: true}, {title: 'Sleeping'}])
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server Started')
